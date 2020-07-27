02b1aa7a9552e0a66b81aec4ef7b06d80db60759
Denilson Fontes Da Cruz <defontescruz@gmail.com>
2020-07-27 19:29:38 -0300

    Project 2/4

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -3,17 +3,51 @@
  */
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+import chess.ChessException;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Progam {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner inp = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
 		
-		System.out.println("");
+		while(true) {
+		try {
+			
+			UI.clearScreen();
+			UI.printBoard(chessMatch.getPieces());
+			
+			System.out.print("\n\nOrigem: ");
+			ChessPosition source = UI.readChessPosition(inp);
+			
+			System.out.print("\nDestino: ");
+			ChessPosition target = UI.readChessPosition(inp);
+			
+			ChessPiece capturedChessPiece = chessMatch.performChessMove(source, target);
 		
-	}
+		}
+		catch(ChessException e){
+			System.out.println(e.getMessage());
+			inp.next();
+		}
+		catch(InputMismatchException e) {
+			System.out.println(e.getMessage());
+			inp.next();
+		}
+		finally {
+			inp.close();
+		}
+		
+		}
 
+	}
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,6 +8,9 @@ public class Board {
 	
 	
 	public Board(Integer rows, Integer columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Error: É necessario que possua pelo menos 1 linha e 1 coluna");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		
@@ -19,29 +22,59 @@ public class Board {
 		return rows;
 	}
 
-
-	public void setRows(Integer rows) {
-		this.rows = rows;
-	}
-
-
 	public Integer getColumns() {
 		return columns;
 	}
 
-
-	public void setColumns(Integer columns) {
-		this.columns = columns;
-	}
-	
 	public Piece piece(Integer row, Integer column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Error: Posição inexistente");
+		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Error: Posição inexistente");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
+	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("Error: Ja existe uma peça na posição atual");
+		}
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
 	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Error: Posição inexistente");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		
+		return aux;
+	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Error: Posição inexistente");		
+		}
+		return piece(position) != null;
+	}
 	
 }
